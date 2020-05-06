d41221a78affdd93cd7f28f9ef6a292d7ec1c807
raghavnautiyal <raghavnautiyal26@gmail.com>
2020-05-06 16:44:01 +0530

    Minor Modifications Added ability to exit from the prompt Added version number Minor bug fixes

--- shell.py
+++ shell.py
@@ -1,8 +1,17 @@
 import dreamscript
 
 while True:
+    print("\nDreamscript Version 0.1\n")
+    print("Type in 'exit' to exit the prompt\n")
     raw_text = input("DreamScript>  ")
     result, error = dreamscript.run('<stdin>', raw_text)
+    
+    command = 'exit'
+
+    if command == raw_text.lower():
+        exit(0)
 
     if error: print(error.as_string())
     else: print(result)
+
+    
