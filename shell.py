9c6ea1eede4edbae5c6d29486d9dbeb402c6cc8c
raghavnautiyal <raghavnautiyal26@gmail.com>
2020-05-11 18:29:36 +0530

    Bugs squashed

--- shell.py
+++ shell.py
@@ -5,12 +5,17 @@ print("Type in 'exit' to exit the prompt\n")
 
 while True:
     raw_text = input("DreamScript>  ")
+    if raw_text.strip() == "": continue
     result, error = dreamscript.run('<stdin>', raw_text)
 
     command = 'exit'
 
     if command == raw_text.lower():
         exit()
-
+        
     if error: print(error.as_string())
-    elif result: print(repr(result))
+    elif result: 
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
