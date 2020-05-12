d4c37245f033262eccd2428618b2a7aaf9c2eb26
raghavnautiyal <raghavnautiyal26@gmail.com>
2020-05-13 00:10:15 +0530

    Squashed minor bugs

--- shell.py
+++ shell.py
@@ -4,7 +4,7 @@ print("\nDreamscript Beta (Version 0.1)\n")
 print("Type in 'exit' to exit the prompt\n")
 
 while True:
-    raw_text = input("DreamScript>  ")
+    raw_text = input("Dreamscript>  ")
     if raw_text.strip() == "": continue
     result, error = dreamscript.run('<stdin>', raw_text)
 
