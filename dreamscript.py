f4571dfef55df7b3502a8c96d90f4a9ecac64928
raghavnautiyal <raghavnautiyal26@gmail.com>
2020-05-13 16:53:16 +0530

    YOU CAN RUN CODE

--- dreamscript.py
+++ dreamscript.py
@@ -186,6 +186,8 @@ class Lexer:
                 while self.current_char != None:
                         if self.current_char in ' \t':
                                 self.advance()
+                        elif self.current_char in '#':
+                                self.skip_comment()
                         elif self.current_char in DIGITS:
                                 tokens.append(self.make_number())
                         elif self.current_char in ';\n':
@@ -362,6 +364,14 @@ class Lexer:
                 tok_type = TT_GTE
 
             return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+        def skip_comment(self):
+            self.advance()
+
+            while self.current_char != '\n':
+                self.advance()
+
+            self.advance()
         
 
 
@@ -1817,6 +1827,61 @@ class BuiltInFunction(BaseFunction):
         
     execute_extend.arg_names = ["listA", "listB"]
 
+    def execute_len(self, exec_ctx):
+        list_ = exec_ctx.symbol_table.get("list")
+        
+        if not isinstance(list_, List):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "Argument must be list",
+                exec_ctx
+            ))
+            
+        return RTResult().success(Number(len(list_.elements)))
+
+    execute_len.arg_names = ["list"]
+
+    def execute_run(self, exec_ctx):
+        fn = exec_ctx.symbol_table.get("fn")
+        
+        if not isinstance(fn, String):
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                "Second argument must be string",
+                exec_ctx
+            ))
+            
+        fn = fn.value
+        
+        try:
+            with open(fn, "r") as f:
+                script = f.read()
+                
+        except Exception as e:
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                f"Failed to load script \"{fn}\"\n" + str(e),
+                exec_ctx
+                ))
+        
+        _, error = run(fn, script)
+        
+        if error:
+            return RTResult().failure(RTError(
+                self.pos_start, self.pos_end,
+                f"Failed to finish executing script \"{fn}\"\n" +
+                error.as_string(),
+                exec_ctx
+                
+            ))
+        
+        return RTResult().success(Number.null)
+
+    execute_run.arg_names = ["fn"]
+
+
+
+
 BuiltInFunction.print       = BuiltInFunction("print")
 BuiltInFunction.input       = BuiltInFunction("input")
 BuiltInFunction.input_integer   = BuiltInFunction("input_integer")
@@ -1831,6 +1896,8 @@ BuiltInFunction.cos      = BuiltInFunction("cos")
 BuiltInFunction.tan      = BuiltInFunction("tan")
 BuiltInFunction.pop         = BuiltInFunction("pop")
 BuiltInFunction.extend      = BuiltInFunction("extend")
+BuiltInFunction.len         = BuiltInFunction("len")
+BuiltInFunction.run      = BuiltInFunction("run")
 
 class List(Value):
     def __init__(self, elements):
@@ -2207,7 +2274,8 @@ global_symbol_table.set("is_function", BuiltInFunction.is_function)
 global_symbol_table.set("add", BuiltInFunction.append)
 global_symbol_table.set("remove", BuiltInFunction.pop)
 global_symbol_table.set("join", BuiltInFunction.extend)
-
+global_symbol_table.set("length", BuiltInFunction.len)
+global_symbol_table.set("run", BuiltInFunction.run)
 
 def run(fn, text):
         # Generate tokens
