b364ae9a35d63258a84130b323919bdca12ab36a
raghavnautiyal <raghavnautiyal26@gmail.com>
2020-05-21 22:43:30 +0530

    The dreamscript text editor is reaaalllll

--- src/Dreamshell.py
+++ src/Dreamshell.py
@@ -1,6 +1,6 @@
 import dreamscript
 import tkinter as tk
-from tkinter import Tk, scrolledtext, Menu, filedialog, messagebox
+from tkinter import Tk, scrolledtext, Menu, filedialog, messagebox, END
 import sys
 import re
 from code import InteractiveConsole
@@ -288,86 +288,156 @@ class TextConsole(tk.Text):
             self.insert('insert', '\n')
             self.prompt()
 
+class Menubar:
+
+    def __init__(self, parent):
+        font_specs = ("sans", 14)
+
+        menubar = tk.Menu(parent.master, font=font_specs)
+        parent.master.config(menu=menubar)
+
+        file_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
+        file_dropdown.add_command(label="New File",
+                                  accelerator="Ctrl+N",
+                                  command=parent.new_file)
+        file_dropdown.add_command(label="Open File",
+                                  accelerator="Ctrl+O",
+                                  command=parent.open_file)
+        file_dropdown.add_command(label="Save",
+                                  accelerator="Ctrl+S",
+                                  command=parent.save)
+        file_dropdown.add_command(label="Save As",
+                                  accelerator="Ctrl+Shift+S",
+                                  command=parent.save_as)
+        file_dropdown.add_separator()
+        file_dropdown.add_command(label="Exit",
+                                  command=parent.master.destroy)
+
+        about_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
+        about_dropdown.add_command(label="Release Notes",
+                                   command=self.show_release_notes)
+        about_dropdown.add_separator()
+        about_dropdown.add_command(label="About",
+                                   command=self.show_about_message)
+
+        menubar.add_cascade(label="File", menu=file_dropdown)
+        menubar.add_cascade(label="About", menu=about_dropdown)
+
+    def show_about_message(self):
+        box_title = "About Dreamscript"
+        box_message = "The Dreamscript IDE!"
+        messagebox.showinfo(box_title, box_message)
+
+    def show_release_notes(self):
+        box_title = "Release Notes"
+        box_message = "Dreamshell - Beta"
+        messagebox.showinfo(box_title, box_message)
+
+
+class Statusbar:
+
+    def __init__(self, parent):
+
+        font_specs = ("courier", 12)
+        
+        self.status = tk.StringVar()
+        self.status.set("Dreamshell - Beta")
+
+        label = tk.Label(parent.textarea, textvariable=self.status, fg="black",
+                         bg="lightgrey", anchor='sw', font=font_specs)
+        label.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    def update_status(self, *args):
+        if isinstance(args[0], bool):
+            self.status.set("Your File Has Been Saved!")
+        else:
+            self.status.set("Dreamshell - Beta")
+
+
+class DreamText:
+
+    def __init__(self, master):
+        master.title("Untitled - Dreamshell")
+        master.geometry("1200x700")
+
+        font_specs = ("courier", 14)
+
+        self.master = master
+        self.filename = None
+
+        self.textarea = tk.Text(master, font=font_specs)
+        self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
+        self.textarea.configure(yscrollcommand=self.scroll.set)
+        self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.menubar = Menubar(self)
+        self.statusbar = Statusbar(self)
+
+        self.bind_shortcuts()
+
+    def set_window_title(self, name=None):
+        if name:
+            self.master.title(name + " - Dreamshell")
+        else:
+            self.master.title("Untitled - Dreamshell")
+
+    def new_file(self, *args):
+        self.textarea.delete(1.0, tk.END)
+        self.filename = None
+        self.set_window_title()
+
+    def open_file(self, *args):
+        self.filename = filedialog.askopenfilename(
+            defaultextension=".txt",
+            filetypes=[("Dreamscript Files", "*.drm")])
+        if self.filename:
+            self.textarea.delete(1.0, tk.END)
+            with open(self.filename, "r") as f:
+                self.textarea.insert(1.0, f.read())
+            self.set_window_title(self.filename)
+    
+    def save(self, *args):
+        if self.filename:
+            try:
+                textarea_content = self.textarea.get(1.0, tk.END)
+                with open(self.filename, "w") as f:
+                    f.write(textarea_content)
+                self.statusbar.update_status(True)
+            except Exception as e:
+                print(e)
+        else:
+            self.save_as()
+
+    def save_as(self, *args):
+        try:
+            new_file = filedialog.asksaveasfilename(
+                initialfile="Untitled.drm",
+                defaultextension=".txt",
+                filetypes=[("Dreamscript Files", "*.drm")])
+            textarea_content = self.textarea.get(1.0, tk.END)
+            with open(new_file, "w") as f:
+                f.write(textarea_content)
+            self.filename = new_file
+            self.set_window_title(self.filename)
+            self.statusbar.update_status(True)
+        except Exception as e:
+            print(e)
+
+    def bind_shortcuts(self):
+        self.textarea.bind('<Control-n>', self.new_file)
+        self.textarea.bind('<Control-o>', self.open_file)
+        self.textarea.bind('<Control-s>', self.save)
+        self.textarea.bind('<Control-S>', self.save_as)
+        self.textarea.bind('<Key>', self.statusbar.update_status)
+        
+
 
 if __name__ == '__main__':
     
     root = tk.Tk()
     console = TextConsole(root)
-    textArea = scrolledtext.ScrolledText(width = 500, height = 500)
-    
-    def newFile():
-        textArea = scrolledtext.ScrolledText(width = 500, height = 500)
-        if len(textArea.get('1.0', 'end' + '-1c')) > 0:
-            if messagebox.askyesno("Save", "Do you wish to save?"):
-                saveFile()
-        newWindow = tk.Toplevel(scrolledtext.ScrolledText(width = 500, height = 500))
-        newWindow.title("Dreamscript Editor")
-        newWindow.geometry("500x500")  
-        newFile.newtextArea = scrolledtext.ScrolledText(newWindow, width = 500, height = 500)
-        newFile.newtextArea.pack()
-        menu = Menu(root)
-        root.config(menu=menu)
-        fileMenu = Menu(menu)
-    
-        menu.add_cascade(label="File", menu = fileMenu)
-        fileMenu.add_command(label="New File", command=newFile)
-        fileMenu.add_command(label="Open...", command=openFile)
-        fileMenu.add_command(label="Save", command=saveFile)
-        fileMenu.add_separator()
-        fileMenu.add_command(label="Quit", command=exitRoot)
-
-        helpMenu = Menu(menu)
-        menu.add_cascade(label="Help", menu = helpMenu)
-
-        abtmenu = Menu(menu)
-        menu.add_cascade(label="About",  menu = abtmenu)
-        root.mainloop()
-
-
-    def openFile():
-        file = filedialog.askopenfile(parent=root, title='Select a .drm file', filetypes=(("Dreamscript File", "*.drm"),))
-
-        if file != None:
-            contents = file.read()
-            newFile.newtextArea.insert('1.0', contents)
-            file.close()
-
-    def openFilefromprompt():
-        file = filedialog.askopenfile(parent=root, title='Select a .drm file', filetypes=(("Dreamscript File", "*.drm"),))
-        newFile()
-        if file != None:
-            contents = file.read()
-            newFile.newtextArea.insert('1.0', contents)
-            file.close()
-
-    def saveFile():
-        file = filedialog.asksaveasfile(mode='w')
-
-        if file != None:
-            data = newFile.newtextArea.get('1.0', 'end' + '-1c')
-            file.write(data)
-            file.close()
-
-    def exitRoot():
-        if tk.messagebox.askyesno("Quit", "Are you sure you want to quit?"):
-            root.destroy()
-
-    menu = Menu(root)
-    root.config(menu=menu)
-    fileMenu = Menu(menu)
-    menu.add_cascade(label="File", menu = fileMenu)
-    fileMenu.add_command(label="New File", command=newFile)
-    fileMenu.add_command(label="Open...", command=openFilefromprompt)
-    fileMenu.add_separator()
-    fileMenu.add_command(label="Quit", command=exitRoot)
-
-    helpMenu = Menu(menu)
-    menu.add_cascade(label="Help", menu = helpMenu)
-
-    abtmenu = Menu(menu)
-    menu.add_cascade(label="About",  menu = abtmenu)
-    #menu.add_cascade(label="About",  command = about)
-    root.title("Dreamscript Beta (Version 0.1) - Shell")
+    dt = DreamText(root)
     console.pack(fill='both', expand=True)
     root.mainloop()
 
